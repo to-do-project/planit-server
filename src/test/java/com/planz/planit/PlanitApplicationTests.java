0e126707cd1ba6794afb6e7da1928f3175e373ab
eunjee <rira9817@gmail.com>
2022-03-17 21:27:34 +0900

    initialize

---
+++ src/test/java/com/planz/planit/PlanitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planz.planit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
