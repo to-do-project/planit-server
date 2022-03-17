c5e402e36c1e7f80d362dac15a37ce80dbbfd2af
YoumHyeJi <50475178+YoumHyeJi@users.noreply.github.com>
2022-03-17 22:11:09 +0900

    Update deploy.sh
    app name 수정

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/deploy
 cd $REPOSITORY
 
-APP_NAME=planit-server
+APP_NAME=planit
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
