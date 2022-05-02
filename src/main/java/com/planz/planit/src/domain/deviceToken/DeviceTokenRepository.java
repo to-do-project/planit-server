0e7ae97332d709dc66731a0afb2c1af204f416a4
YoumHyeJi <50475178+YoumHyeJi@users.noreply.github.com>
2022-05-02 23:45:14 +0900

    회원가입 에러 수정 (#13)

--- src/main/java/com/planz/planit/src/domain/deviceToken/DeviceTokenRepository.java
+++ src/main/java/com/planz/planit/src/domain/deviceToken/DeviceTokenRepository.java
@@ -1,10 +1,11 @@
 package com.planz.planit.src.domain.deviceToken;
 
 import com.planz.planit.src.domain.user.User;
-import io.lettuce.core.dynamic.annotation.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,10 +13,10 @@ import java.util.List;
 public interface DeviceTokenRepository extends JpaRepository<DeviceToken,Long> {
 
     //특정 디바이스 토큰 객체 가져오기 (userId+deviceToken)
-    @Query("select d from DeviceToken d where d.user.userId = :userId and d.deviceToken= :deviceToken")
+    @Query("select d from DeviceToken d where d.user.userId is :userId and d.deviceToken is :deviceToken")
     public DeviceToken findDeviceTokenByUserIdAndDeviceToken(@Param("userId")Long userId,@Param("deviceToken") String deviceToken);
     //기존 디바이스 토큰이 존재하는지 확인
-    public boolean existsByUserAndDeviceToken(User user,@Param("deviceToken") String deviceToken);
+    public boolean existsByUserAndDeviceToken(User user, String deviceToken);
 
     // 해당 userId가 가진 모든 디바이스 토큰 삭제하기 => 혜지 추가
     @Transactional
