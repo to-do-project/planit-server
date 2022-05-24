a275bcc83ac1a95579b59e9a53f8a6514e97b9c1
YoumHyeJi <50475178+YoumHyeJi@users.noreply.github.com>
2022-05-24 17:25:59 +0900

    fetch upstream (#1)
    * dev 1차 배포
    
    * yml error
    
    * Update PlanitApplicationTests.java
    
    * Update PlanitApplicationTests.java
    
    * Update PlanitApplicationTests.java
    
    * yml 파일 테스트
    
    yml 파일 사라지는지 테스트
    
    * test 삭제
    
    test 삭제
    
    Co-authored-by: eunjee <rira9817@gmail.com>

--- src/test/java/com/planz/planit/PlanitApplicationTests.java
+++ src/test/java/com/planz/planit/PlanitApplicationTests.java
@@ -3,10 +3,10 @@ package com.planz.planit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PlanitApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
